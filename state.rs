15934547aecb83292550446b0e68ade8260ec0ee
Elijah Mooring <vehmloewff@gmail.com>
2024-03-12 10:22:36 -0700

    ref: reform what state is

--- state.rs
+++ state.rs
@@ -1,120 +1,22 @@
-use crate::ViewId;
-use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
-use serde_json::Value;
+use std::any::Any;
 
-#[derive(Debug, Serialize, Deserialize, JsonSchema)]
-pub enum UpstreamMessage {
-	OnPagePathChanged {
-		new_path: String,
-	},
-	ActionTrigger {
-		element_id: ViewId,
-		event_name: String,
-		data: ElementActionPayload,
-	},
-}
+#[derive(Debug)]
+pub struct State(Option<Box<dyn Any>>);
 
-#[derive(Debug, Serialize, Deserialize, JsonSchema)]
-pub enum DownstreamMessage {
-	SetChildren { id: ViewId, children: Vec<GenericElement> },
-	SetRootChildren { children: Vec<GenericElement> },
-	SetAttribute { id: ViewId, name: String, value: Option<String> },
-	AddCssChunk { id: ViewId, content: String },
-	DeleteCssChunk { id: ViewId },
-	SetTextContent { id: ViewId, content: String },
-	SetPagePath { path: String },
-	SetPageTitle { title: String },
-	ShowError { message: String },
-	CancelLoader { action_id: ViewId },
-}
+impl State {
+	pub fn new() -> State {
+		State(None)
+	}
 
-#[derive(Debug, Serialize, Deserialize, JsonSchema)]
-pub struct GenericElement {
-	pub id: Option<ViewId>,
-	pub tag: String,
-	pub actions: Vec<ActionDefinition>,
-	pub children: Vec<GenericElement>,
-	pub attributes: Vec<(String, String)>,
-	pub text_content: Option<String>,
-	pub delete_after_ms: Option<u32>,
-}
-
-impl GenericElement {
-	pub fn new(tag: impl Into<String>) -> GenericElement {
-		GenericElement {
-			id: None,
-			tag: tag.into(),
-			actions: Vec::new(),
-			children: Vec::new(),
-			attributes: Vec::new(),
-			text_content: None,
-			delete_after_ms: None,
+	pub unsafe fn get<T: Default + 'static>(&mut self) -> &mut T {
+		if let None = self.0 {
+			self.0.replace(Box::new(T::default()));
 		}
+
+		let any = self.0.as_mut().unwrap_unchecked();
+		let any_ptr = any.as_mut() as *mut dyn Any;
+		let t_ptr = any_ptr as *mut T;
+
+		&mut *t_ptr
 	}
-
-	pub fn action(mut self, action: ActionDefinition) -> GenericElement {
-		self.actions.push(action);
-
-		self
-	}
-
-	pub fn attribute(mut self, name: impl Into<String>, value: impl Into<String>) -> GenericElement {
-		self.attributes.push((name.into(), value.into()));
-
-		self
-	}
-}
-
-#[derive(Debug, Serialize, Deserialize, JsonSchema)]
-pub struct ActionDefinition {
-	pub loader_behavior: ActionLoaderBehavior,
-	pub id: ViewId,
-	pub event: String,
-	pub payload_kind: ElementActionPayloadKind,
-}
-
-impl ActionDefinition {
-	pub fn new(id: ViewId, event: impl Into<String>) -> ActionDefinition {
-		ActionDefinition {
-			loader_behavior: ActionLoaderBehavior::Hide,
-			id,
-			event: event.into(),
-			payload_kind: ElementActionPayloadKind::Nothing,
-		}
-	}
-
-	pub fn loader_behavior(mut self, behavior: ActionLoaderBehavior) -> ActionDefinition {
-		self.loader_behavior = behavior;
-
-		self
-	}
-
-	pub fn payload_kind(mut self, payload_kind: ElementActionPayloadKind) -> ActionDefinition {
-		self.payload_kind = payload_kind;
-
-		self
-	}
-}
-
-#[derive(Debug, Serialize, Deserialize, JsonSchema)]
-pub enum ActionLoaderBehavior {
-	Show,
-	Hide,
-}
-
-#[derive(Debug, Serialize, Deserialize, JsonSchema)]
-pub enum ElementActionPayload {
-	InputValue { value: String },
-	Nothing,
-	Switch { value: bool },
-	Dynamic { value: Value },
-}
-
-#[derive(Debug, Serialize, Deserialize, JsonSchema)]
-pub enum ElementActionPayloadKind {
-	InputValue,
-	Nothing,
-	Switch,
-	Dynamic,
 }
